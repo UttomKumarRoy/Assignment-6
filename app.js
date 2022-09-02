007e8a99e6072c90689ea8b9a99382922fa0e782
Uttom <uttom.teacher.gps2@gmail.com>
2022-09-03 01:52:47 +0600

    News menu added

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("First commit");
+//news menu section 
+fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => displayMenu(data.data.news_category))
+    .catch(err => console.log(err))
+const menuContainer = document.getElementById('menu-container');
+const displayMenu = (categories) => {
+    console.log(categories);
+    categories.forEach(category => {
+        const a = document.createElement('a');
+        a.innerText = `${category.category_name}`;
+        a.classList.add('col');
+        a.setAttribute('id', '${category.category_id}')
+        menuContainer.appendChild(a);
+    });
+}
