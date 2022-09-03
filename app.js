28a92a12ad6a1a2de9ca697cd755030c8b51654a
Uttom <uttom.teacher.gps2@gmail.com>
2022-09-03 09:13:46 +0600

    All News added dynamically

--- app.js
+++ app.js
@@ -5,48 +5,45 @@ fetch('https://openapi.programming-hero.com/api/news/categories')
     .catch(err => console.log(err))
 const menuContainer = document.getElementById('menu-container');
 const displayMenu = (categories) => {
-        categories.forEach(category => {
-            const a = document.createElement('a');
-            a.innerText = `${category.category_name}`;
-            a.classList.add('col');
-            a.setAttribute('id', `${category.category_id}`);
-            a.setAttribute('href', '#');
-            menuContainer.appendChild(a);
-        });
-    }
-    //news section 
-setTimeout(() => {
-    document.getElementById('01').addEventListener('click', function() {
-        fetch('https://openapi.programming-hero.com/api/news/category/01')
-            .then(res => res.json())
-            .then(data => displayNews(data.data))
-            .catch(err => console.log(err))
-    })
+    categories.forEach(category => {
+        const a = document.createElement('a');
+        a.innerText = `${category.category_name}`;
+        a.classList.add('col');
+        a.setAttribute('id', `${category.category_id}`);
+        a.setAttribute('href', '#');
+        menuContainer.appendChild(a);
+        document.getElementById(category.category_id).addEventListener('click', () => {
+            allNews(category.category_id);
+        })
+    });
+}
 
-    document.getElementById('02').addEventListener('click', function() {
-        fetch('https://openapi.programming-hero.com/api/news/category/02')
-            .then(res => res.json())
-            .then(data => displayNews(data.data))
-            .catch(err => console.log(err))
-    })
 
-    document.getElementById('03').addEventListener('click', function() {
-        fetch('https://openapi.programming-hero.com/api/news/category/03')
-            .then(res => res.json())
-            .then(data => displayNews(data.data))
-            .catch(err => console.log(err))
-    })
+//news section 
 
-    const displayNews = (newsAll) => {
-        console.log(newsAll);
-        const newsContainer = document.getElementById('news-container');
-        newsContainer.innerHTML = '';
-        newsAll.forEach((news) => {
-            const { title, total_view, author, image_url, details, thumbnail_url } = news;
-            const { name, img } = author;
-            const div = document.createElement('div');
-            div.classList.add('row');
-            div.innerHTML = `
+
+const allNews = (id) => {
+    console.log(id);
+
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNews(data.data))
+        .catch(err => console.log(err))
+}
+
+
+
+const displayNews = (newsAll) => {
+    console.log(newsAll);
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = '';
+    newsAll.forEach((news) => {
+        const { title, total_view, author, image_url, details, thumbnail_url } = news;
+        const { name, img } = author;
+        const div = document.createElement('div');
+        div.classList.add('row');
+        div.innerHTML = `
                         <div class="col-3">
                         <img class="img-fluid" src="${image_url}" alt="">
                         </div>
@@ -54,14 +51,13 @@ setTimeout(() => {
                         <h3> ${title}</h3>
                         <p>${details}</p>
                         <p>
-                            <img class="img-fluid" src="${img}" alt="">
+                            <img class="author-img" src="${img}" alt="">
                             <span>${name}</span>
                             <span><i class="fa-thin fa-eye"></i>${total_view}</span>
                             <button>Author Info</button>
                         </p>
                         </div>                     
 `
-            newsContainer.appendChild(div);
-        })
-    }
-}, 1000);
+        newsContainer.appendChild(div);
+    })
+}
