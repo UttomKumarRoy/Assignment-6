4fe2f2fa795ebc388cd7e1ebf54261bf0c6474d8
Uttom <uttom.teacher.gps2@gmail.com>
2022-09-03 10:45:56 +0600

    Modal added

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ const displayMenu = (categories) => {
 
 
 const allNews = (id) => {
-    console.log(id);
-
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     fetch(url)
         .then(res => res.json())
@@ -35,11 +33,10 @@ const allNews = (id) => {
 
 
 const displayNews = (newsAll) => {
-    console.log(newsAll);
     const newsContainer = document.getElementById('news-container');
     newsContainer.innerHTML = '';
     newsAll.forEach((news) => {
-        const { title, total_view, author, image_url, details, thumbnail_url } = news;
+        const { _id, title, total_view, author, image_url, details, thumbnail_url } = news;
         const { name, img } = author;
         const div = document.createElement('div');
         div.classList.add('row');
@@ -54,10 +51,47 @@ const displayNews = (newsAll) => {
                             <img class="author-img" src="${img}" alt="">
                             <span>${name}</span>
                             <span><i class="fa-thin fa-eye"></i>${total_view}</span>
-                            <button>Author Info</button>
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" id='${_id}'>Author Info</button>
                         </p>
                         </div>                     
 `
         newsContainer.appendChild(div);
+        document.getElementById(_id).addEventListener('click', () => {
+            showModal(_id);
+        })
     })
+}
+
+//Modal of details news info 
+const showModal = (_id) => {
+    const url = ` https://openapi.programming-hero.com/api/news/${_id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayNewsDetails(data.data[0]))
+        .catch(err => console.log(err))
+}
+
+const displayNewsDetails = (news) => {
+    console.log(news);
+    const { title } = news;
+    const modal = document.getElementById('modal');
+    modal.innerHTML = `
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">${title}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
+    `
 }
